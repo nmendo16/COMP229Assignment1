3d3b5c259e6beb5700008363177d9abfab202901
nmendo16 <nmendo16@my.centennialcollege.ca>
2024-09-29 01:13:41 -0400

    Changes to image paths for the third time, added the (./src/) to their path

--- src/about.jsx
+++ src/about.jsx
@@ -5,14 +5,14 @@ export default function About() {
         <br />
         <br />
         <br />
-        <img id="face" src="naomi.jpeg" alt="Naomi's Picture" />
+        <img id="face" src="./src/naomi.png" alt="Naomi's Picture" />
           
         <p>I'm Naomi Mendoza. I'm a student of software engineering,
         </p><p>specializing in <b>Game Programming</b> 🎮,</p>
               <p> I possess a unique skillset 💎 that spans across various digital disciplines.</p>
               <p> Enthusiastic to find solutions 🔎 with a <b>collaborative</b> and <b>results-focused mindset.</b></p>
              <br />
-              <a href="Resume.pdf" target="_blank" rel="noopener noreferrer">Naomi Mendoza's Resume</a>
+              <a href="./src/Resume.pdf" target="_blank" rel="noopener noreferrer">Naomi Mendoza's Resume</a>
       
               
 
